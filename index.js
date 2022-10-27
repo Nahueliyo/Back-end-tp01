610a100b26d9462d306042854ecdc14869dfdba0
Nahuel Castello <nahuel250797@gmail.com>
2022-10-27 17:17:37 -0300

    commit-1.1

--- index.js
+++ index.js
@@ -5,9 +5,35 @@ class Usuario {
         this.libros = libros;
         this.mascotas = mascotas;
     }
+
+    getFullName(){
+        return console.log `El nombre del usuario es ${this.name} y su apellido es ${this.apellido}`
+    }
+
+    anadirMascotas (){
+        
+    }
+
+    obtenerLibros(){
+        console.log(`Lista de libros: ${this.libros.obtenerLibros}`)
+    }
+
+
+    // agregarLibro(nombreLibro){
+    //     this.libros.push(nombreLibro)
+    // }
+    // listarLibros(){
+    //     return this.libros.map()
+    // }
+
+    // agregarMascotas(nombreMascota){
+    //     this.mascotas.push(nombreMascota)
+    // }
+
+
 }
 
-let usuario = new Usuario('','','','')
+// let usuario = new Usuario('','','','')
 
 
 
