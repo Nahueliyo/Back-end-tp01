8d374bf24af4bea9d843d0488725bd59899d90df
Nahuel Castello <nahuel250797@gmail.com>
2022-10-27 16:47:18 -0300

    commit-1.0

---
+++ index.js
@@ -0,0 +1,30 @@
+class Usuario {
+    constructor (nombre, apellido, libros, mascotas){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+}
+
+let usuario = new Usuario('','','','')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
